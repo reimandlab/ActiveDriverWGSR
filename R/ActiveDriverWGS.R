bf7b3d74ed4f8f2a58cacd1d5cccbb262fb65cc2
Helen <helen@Helens-iMac.local>
2018-12-29 00:03:10 -0500

    Started working on a vignette

--- R/ActiveDriverWGS.R
+++ R/ActiveDriverWGS.R
@@ -16,8 +16,8 @@ require(parallel)
 #'     \item{chr}{autosomal chromosomes as chr1 to chr22 and sex chromosomes as chrX and chrY}
 #'     \item{start}{the start position of the element in base 0 coordinates (BED format)}
 #'     \item{end}{the end position of the element in base 0 coordinates (BED format)}
-#'     \item{id}{the element identifier - if the element contains multiple segments such as exons, 
-#'     each segment should be a separate row with the segment coordinates 
+#'     \item{id}{the element identifier - if the element contains multiple segments such as exons,
+#'     each segment should be a separate row with the segment coordinates
 #'     and the element identifier as id. Elements can be coding or noncoding such as exons of protein
 #'     coding genes or active enhancers.}
 #' }
@@ -31,10 +31,10 @@ require(parallel)
 #'     phosphosites of protein coding genes in genomic coordinates or transcription factor binding sites of active
 #'     enhancers.}
 #' }
-#' @param window_size An integer indicating the size of the background window in base pairs for which the 
+#' @param window_size An integer indicating the size of the background window in base pairs for which the
 #' mutation rate is expected to remain the same. The default is 50000bps.
 #' @param filter_hyper_MB Hyper-mutated samples carry many passenger mutations and dilute the signal of true drivers.
-#' Samples with a rate greater than \code{filter_hyper_MB} mutations per megabase are excluded. 
+#' Samples with a rate greater than \code{filter_hyper_MB} mutations per megabase are excluded.
 #' The default is 30 mutations per megabase.
 #' @param recovery.dir The directory for storing recovery files. If no directory is specified, ActiveDriverWGS will write
 #' recovery files in the current directory. If the directory does not exist, ActiveDriverWGS will create the directory.
@@ -63,23 +63,23 @@ require(parallel)
 #' TODO Format all results
 #' TODO Add sample data
 #' TODO Check that the result columns are actually correct
-ActiveDriverWGS = function(mutations, 
-                           elements, 
-                           sites = NULL, 
+ActiveDriverWGS = function(mutations,
+                           elements,
+                           sites = NULL,
                            window_size = 50000,
                            filter_hyper_MB = 30,
                            recovery.dir = "",
                            mc.cores = 1){
-  
+
   # Verifying Format for window_size
   if (!(length(window_size) == 1 && is.numeric(window_size) && window_size > 0)) stop("window size must be a positive integer")
 
   # Verifying Format for mc.cores
   if (!(length(mc.cores) == 1 && is.numeric(mc.cores) && mc.cores > 0)) stop("mc.cores must be a positive integer")
-  
+
   # Verifying Format for filter_hyper_MB
   if (!(length(filter_hyper_MB) == 1 && is.numeric(filter_hyper_MB) && filter_hyper_MB > 0)) stop("filter_hyper_MB must be a positive integer")
-  
+
   # Verifying Format for recovery.dir
   if(!is.character(recovery.dir) | length(recovery.dir) != 1) stop("recovery.dir must be a string")
   if(recovery.dir != ""){
@@ -91,7 +91,7 @@ ActiveDriverWGS = function(mutations,
   if(!endsWith(recovery.dir, "[/]")){
     recovery.dir = paste0(recovery.dir, "/")
   }
-  
+
   # Verifying Format for Mutations
   # legal_dna = c('A', 'T', 'C', 'G')
   if (!is.data.frame(mutations)) stop("mutations must be a data frame")
@@ -102,13 +102,13 @@ ActiveDriverWGS = function(mutations,
   if (!(is.integer(mutations$pos1) && is.integer(mutations$pos2))) stop("pos1 and pos2 must be integers")
   # if (!(all(mutations$ref %in% legal_dna) && all(mutations$alt %in% legal_dna))) stop("Reference and alternate alleles must be A, T, C or G")
   if (!(is.character(mutations$patient))) stop("patient identifier must be a string")
-  
+
   # Creating gr_muts
   mutations = format_muts(mutations, filter_hyper_MB = filter_hyper_MB)
-  gr_muts = GRanges(mutations$chr, 
+  gr_muts = GRanges(mutations$chr,
                     IRanges(mutations$pos1, mutations$pos2), mcols=mutations[,c("patient", "tag")])
   save(gr_muts, file=paste0(recovery.dir,"/gr_muts.rsav"))
-  
+
   # Verifying Format for Elements
   if (!is.data.frame(elements)) stop("elements must be a data frame")
   if (!all(c("chr", "start", "end", "id") %in% colnames(elements))) stop("elements must contain the following columns: chr, start, end & id")
@@ -117,12 +117,12 @@ ActiveDriverWGS = function(mutations,
   if (!all(elements$chr %in% seqnames(Hsapiens)[1:24])) stop("Only the 22 autosomal and 2 sex chromosomes may be used at this time. Note that chr23 should be formatted as chrX and chr24 should be formatted as chrY")
   if (!(is.integer(elements$start) && is.integer(elements$end))) stop("start and end must be integers")
   if (!(is.character(elements$id))) stop("element identifier must be a string")
-  
+
   # Creating elements_gr
   gr_element_coords = GRanges(elements$chr,
                               IRanges(elements$start, elements$end),
                               mcols = elements[,c("id")])
-  
+
   # Verifying Format for Sites
   if(!is.null(sites)){
     if (!is.data.frame(sites)) stop("sites must be a data frame")
@@ -132,17 +132,17 @@ ActiveDriverWGS = function(mutations,
     if (!all(sites$chr %in% seqnames(Hsapiens)[1:24])) stop("Only the 22 autosomal and 2 sex chromosomes may be used at this time. Note that chr23 should be formatted as chrX and chr24 should be formatted as chrY")
     if (!(is.integer(sites$start) && is.integer(sites$end))) stop("start and end must be integers")
     if (!(is.character(sites$id))) stop("site identifier must be a string")
-    
+
     gr_site_coords = GRanges(sites$chr,
                              IRanges(sites$start, sites$end))
   }else{
     gr_site_coords = GRanges(c(seqnames=NULL,ranges=NULL,strand=NULL))
   }
-  
+
   # Running ADWGS Test
   all_results = NULL
   not_done = sort(unique(gr_element_coords$mcols))
-  
+
   recovered_results = NULL
   recovered_result_numbers = c()
   if (!is.na(recovery_dir)) {
@@ -156,10 +156,10 @@ ActiveDriverWGS = function(mutations,
     }))
     recovered_result_numbers = recovered_results$result_number
   }
-  
+
   cat("Tests to do: ", length(not_done), "\n")
   if (length(recovered_result_numbers) > 0) cat("Tests recovered: ", length(recovered_result_numbers), "\n")
-  
+
   mutated_results = do.call(rbind, parallel::mclapply(1:length(not_done), function(i) {
     if (i %% 100 == 0) cat(i, " elements completed\n")
     if (i %in% recovered_result_numbers) return(NULL)
@@ -175,10 +175,10 @@ ActiveDriverWGS = function(mutations,
     }
     result
   }, mc.cores = mc.cores))
-  
+
   mutated_results = rbind(recovered_results, mutated_results)
-  
+
   # Calculating FDR Element, FDR Site and Ordering from Most to Least Significant
-  
+
   mutated_results
 }
